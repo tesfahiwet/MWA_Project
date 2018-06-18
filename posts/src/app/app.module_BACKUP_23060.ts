7f34c1415b43267d9bbd1c60fdd9d5d20383ca12
Mulugeta-Bisrat <34776292+Mulugeta-Bisrat@users.noreply.github.com>
2018-06-18 16:36:03 -0700

    app module updated

---
+++ posts/src/app/app.module_BACKUP_23060.ts
@@ -0,0 +1,52 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule,
+  MatProgressSpinnerModule,
+  MatPaginatorModule
+} from "@angular/material";
+
+import { AppComponent } from "./app.component";
+import { PostCreateComponent } from "./posts/post-create/post-create.component";
+import { HeaderComponent } from "./header/header.component";
+import { PostListComponent } from "./posts/post-list/post-list.component";
+import { LoginComponent } from "./auth/login/login.component";
+import { SignupComponent } from "./auth/signup/signup.component";
+import { AppRoutingModule } from "./app-routing.module";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    HttpClientModule,
+    MatProgressSpinnerModule,
+<<<<<<< HEAD
+    MatExpansionModule,
+    MatPaginatorModule
+=======
+    MatExpansionModule
+>>>>>>> f01600850f489207b78c2545732cf6a0d3118a99
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
