fd46e085e1eb83696dfb5af8438288ce61f1540e
Mulugeta-Bisrat <34776292+Mulugeta-Bisrat@users.noreply.github.com>
2018-06-19 05:35:48 -0700

    authorization has been  updated

--- posts/backend/routes/posts.js
+++ posts/backend/routes/posts.js
@@ -31,14 +31,15 @@ const storage = multer.diskStorage({
 });
 
 router.post(
-  "",
+  "",checkAuth,
   multer({ storage: storage }).single("image"),
   (req, res, next) => {
     const url = req.protocol + "://" + req.get("host");
     const post = new Post({
       title: req.body.title,
       content: req.body.content,
-      imagePath: url + "/images/" + req.file.filename
+      imagePath: url + "/images/" + req.file.filename,
+      creator: req.userData.userId
     });
     post.save().then(createdPost => {
       res.status(201).json({
@@ -48,12 +49,17 @@ router.post(
           id: createdPost._id
         }
       });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Creating a post failed!"
+      });
     });
-  }
+}
 );
 
 router.put(
-  "/:id",
+  "/:id",checkAuth,
   multer({ storage: storage }).single("image"),
   (req, res, next) => {
     let imagePath = req.body.imagePath;
@@ -66,11 +72,22 @@ router.put(
       title: req.body.title,
       content: req.body.content,
       imagePath: imagePath
+      creator: req.userData.userId
     });
-    console.log(post);
-    Post.updateOne({ _id: req.params.id }, post).then(result => {
-      res.status(200).json({ message: "Update successful!" });
-    });
+
+    Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post)
+      .then(result => {
+        if (result.nModified > 0) {
+          res.status(200).json({ message: "Update successful!" });
+        } else {
+          res.status(401).json({ message: "Not authorized!" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({
+          message: "Couldn't udpate post!"
+        });
+      });
   }
 );
 
@@ -86,10 +103,16 @@ router.get("", (req, res, next) => {
   postQuery.then(documents => {
     fetchedPosts = documents;
     return Post.count();
-  }).then(documents => {
+  }).then(count => {
     res.status(200).json({
       message: "Posts fetched successfully!",
-      posts: documents
+      posts: fetchedPosts,
+      maxPosts: count
+    });
+  })
+  .catch(error => {
+    res.status(500).json({
+      message: "Fetching posts failed!"
     });
   });
 });
@@ -101,14 +124,28 @@ router.get("/:id", (req, res, next) => {
     } else {
       res.status(404).json({ message: "Post not found!" });
     }
+  })
+  .catch(error => {
+    res.status(500).json({
+      message: "Fetching post failed!"
+    });
   });
 });
-
-router.delete("/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(result => {
-    console.log(result);
-    res.status(200).json({ message: "Post deleted!" });
-  });
+router.delete("/:id", checkAuth, (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId })
+    .then(result => {
+      console.log(result);
+      if (result.n > 0) {
+        res.status(200).json({ message: "Deletion successful!" });
+      } else {
+        res.status(401).json({ message: "Not authorized!" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Fetching posts failed!"
+      });
+    });
 });
 
 module.exports = router;
